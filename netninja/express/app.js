5e3a65aefb63710f7a4f270ad617aaea4d093c75
BradJKim <bradjkim03@gmail.com>
2024-06-15 19:06:39 -0700

    install morgan, implemented middleware examples

--- netninja/express/app.js
+++ netninja/express/app.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const morgan = require("morgan");
+
 
 // express app
 const app = express();
@@ -9,6 +11,22 @@ app.set("view engine", "ejs");
 
 // listen for requests
 app.listen(3000);
+
+// middleware and static files
+app.use((req, res, next) => {
+    // insert middleware logic here
+    console.log("first middleware")
+    next()
+});
+
+app.use((req, res, next) => {
+    // insert middleware logic here
+    console.log("next middleware")
+    next()
+});
+
+app.use(express.static("public"));
+app.use(morgan('dev'));
 
 app.get("/", (req, res) => {
     //res.sendFile("./views/index.html", { root: __dirname });
