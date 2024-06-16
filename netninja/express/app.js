5e19c3d65898a7acd902a9edb334a3cbe5c84822
BradJKim <bradjkim03@gmail.com>
2024-06-15 20:15:43 -0700

    implementation with mongoose

--- netninja/express/app.js
+++ netninja/express/app.js
@@ -1,6 +1,18 @@
 const express = require("express");
 const morgan = require("morgan");
+const mongoose = require("mongoose");
+const Blog = require("./models/blog");
 
+// connect to mongodb with mongoose
+// listen for requests after connecting
+const dbURI = "";
+mongoose
+    .connect(dbURI)
+    .then((result) => {
+        console.log("Connected to db");
+        app.listen(3000);
+    })
+    .catch((err) => console.log(err));
 
 // express app
 const app = express();
@@ -9,31 +21,64 @@ const app = express();
 app.set("view engine", "ejs");
 // app.set("views", "myviews");
 
-// listen for requests
-app.listen(3000);
-
 // middleware and static files
 app.use((req, res, next) => {
     // insert middleware logic here
-    console.log("first middleware")
-    next()
+    console.log("first middleware");
+    next();
 });
 
 app.use((req, res, next) => {
     // insert middleware logic here
-    console.log("next middleware")
-    next()
+    console.log("next middleware");
+    next();
 });
 
 app.use(express.static("public"));
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
-app.get("/", (req, res) => {
+// mongoose and mongo sandbox routes
+app.get("/add-blog", async (req, res) => {
+    const blog = new Blog({
+        title: "new blog",
+        snippet: "asdfasdfasdf",
+    });
+
+    try {
+        const result = await blog.save();
+        res.send(result);
+    } catch (err) {
+        console.log(err);
+    }
+});
+
+app.get("/all-blogs", async (req, res) => {
+    try {
+        const result = await Blog.find();
+        res.send(result);
+    } catch (err) {
+        console.log(err);
+    }
+});
+
+app.get("/single-blog", async (req, res) => {
+    try {
+        const result = await Blog.findById("666e561ab23e8a133632f621");
+        res.send(result);
+    } catch (err) {
+        console.log(err);
+    }
+});
+
+// routing
+
+app.get("/", async (req, res) => {
     //res.sendFile("./views/index.html", { root: __dirname });
-    const blogs = [
+    /* const blogs = [
         { title: "blog1", snippet: "aslnwrltnsagasfgass" },
         { title: "blog2", snippet: "asrymtryWOTNWDFGIJNSAGASDF" },
-    ];
+    ]; */
+    const blogs = await Blog.find();
     res.render("index", { title: "home", blogs: blogs }); // using ejs view engine, also passes in title var to view
 });
 
