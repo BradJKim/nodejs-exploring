fd8d7f2c24779d009853d603ddfb727d8929930f
BradJKim <bradjkim03@gmail.com>
2024-06-15 18:32:21 -0700

    reinstall express, incorporated view engines
    installing express

--- netninja/express/app.js
+++ netninja/express/app.js
@@ -3,15 +3,25 @@ const express = require("express");
 // express app
 const app = express();
 
+// register view engine
+app.set("view engine", "ejs");
+// app.set("views", "myviews");
+
 // listen for requests
 app.listen(3000);
 
 app.get("/", (req, res) => {
-    res.sendFile("./views/index.html", { root: __dirname });
+    //res.sendFile("./views/index.html", { root: __dirname });
+    const blogs = [
+        { title: "blog1", snippet: "aslnwrltnsagasfgass" },
+        { title: "blog2", snippet: "asrymtryWOTNWDFGIJNSAGASDF" },
+    ];
+    res.render("index", { title: "home", blogs: blogs }); // using ejs view engine, also passes in title var to view
 });
 
 app.get("/about", (req, res) => {
-    res.sendFile("./views/about.html", { root: __dirname });
+    //res.sendFile("./views/about.html", { root: __dirname });
+    res.render("about");
 });
 
 app.get("/blog", (req, res) => {
@@ -20,8 +30,8 @@ app.get("/blog", (req, res) => {
 
 // redirects
 app.get("/about-us", (req, res) => {
-    res.redirect("./about")
-})
+    res.redirect("./about");
+});
 
 // 404, doesn't match with above paths
 app.use((req, res) => {
